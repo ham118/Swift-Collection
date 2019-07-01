a7df90eeb1db441af99e02f106bf6def6009ba8a
Harsh M <12500640+ham118@users.noreply.github.com>
2019-07-01 14:50:09 +0530

    Added EvaluatingJavaScript.swift

---
+++ EvaluatingJavaScript.swift
@@ -0,0 +1,15 @@
+//This is the code to change font size and avoid text copy functionality from entire html in UIWebView
+  //"textSize" : this is integer variable which you want to set font size
+  func webViewDidFinishLoad(_ webView: UIWebView) {    
+    if (webView.stringByEvaluatingJavaScript(from: "document.readyState") == "complete") {
+      //To avoid to detect touch gesture for disable user selection
+      webView.stringByEvaluatingJavaScript(from: "document.documentElement.style.webkitUserSelect='none';")
+      webView.stringByEvaluatingJavaScript(from: "document.documentElement.style.webkitTouchCallout='none';")
+      //-----
+      
+      //To set font size
+      let jsString = "document.getElementsByTagName('body')[0].style.fontSize='\(textSize)px'"
+      webView.stringByEvaluatingJavaScript(from: jsString)
+      //-----
+    }
+  }
