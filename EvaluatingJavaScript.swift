1dd147a48eaad3e54a7459cbe85760fdbc55d381
HarshM <harsh.mehta118@gmail.com>
2019-11-20 12:29:45 +0530

    Update EvaluatingJavaScript.swift

--- EvaluatingJavaScript.swift
+++ EvaluatingJavaScript.swift
@@ -1,4 +1,4 @@
-  //This is the code to change font size and avoid text copy functionality from entire html in UIWebView | Swift version: 4
+  //This is the code to change font size and avoid text copy functionality from entire html in UIWebView | Swift 4
   //"textSize": this is integer variable which you want to set font size
   func webViewDidFinishLoad(_ webView: UIWebView) {    
     if (webView.stringByEvaluatingJavaScript(from: "document.readyState") == "complete") {
